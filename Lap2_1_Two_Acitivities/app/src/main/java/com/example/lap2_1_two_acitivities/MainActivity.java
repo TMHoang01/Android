63d6d99326dc8757e6ec10f5e763cd419f264752
= <71186945+TMHoang01@users.noreply.github.com>
2022-05-16 23:12:20 +0700

    update to lap 2.2 Activity lifecycle and state

--- Lap2_1_Two_Acitivities/app/src/main/java/com/example/lap2_1_two_acitivities/MainActivity.java
+++ Lap2_1_Two_Acitivities/app/src/main/java/com/example/lap2_1_two_acitivities/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 //    public static final int TEXT_REQUEST = 1;
 
     private EditText edtMessage ;
-    private TextView edtReplyHead;
+    private TextView edtReplyHead = null;
     private TextView edtReply;
 
     final private ActivityResultLauncher<Intent> mActivityLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
@@ -29,36 +31,71 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onActivityResult(ActivityResult result) {
                     Log.d(LOG_TAG, "Btn clicked from Second");
+
+
                     if(result.getResultCode() == RESULT_OK){
                         Intent intent = result.getData();
                         String strReply = intent.getStringExtra(SecondActivity.EXTRA_REPLY);
                         edtReply.setText(strReply);
+                        edtReply.setVisibility(View.VISIBLE);
+                        edtReplyHead.setVisibility(View.VISIBLE);
                     }
-
                 }
             });
 
 
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(edtReplyHead.getVisibility() == View.VISIBLE){
+            outState.putBoolean("reply_visible", true);
+            String strContent =edtReply.getText().toString();
+            outState.putString("reply_content", strContent);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "on Create");
         setContentView(R.layout.activity_main);
         edtMessage = findViewById(R.id.txtSend);
-
         edtReplyHead = findViewById(R.id.text_header_reply);
         edtReply = findViewById(R.id.txt_message_reply);
+        
+//        edtReply.getText();
+
+
+
 
 
         Intent  intentR = getIntent();
 //        mActivityLauncher.launch(intentR);
         edtReply.setText(intentR.getStringExtra(SecondActivity.EXTRA_REPLY));
 
+
+        //        restore the state
+        if(savedInstanceState != null){
+            boolean isVisible = savedInstanceState.getBoolean("reply_visible");
+            if(isVisible){
+                edtReplyHead.setVisibility(View.VISIBLE);
+                edtReply.setVisibility(View.VISIBLE);
+                String content = savedInstanceState.getString("reply_content");
+                edtReply.setText(content);
+            }
+
+        }
+
+
+//        send Activity second
         Button btnSend = findViewById(R.id.btn_send);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(LOG_TAG, "Btn clicked from Main! ");
+//                Log.d(LOG_TAG, "Btn clicked from Main! ");
                 Intent intent = new Intent(MainActivity.this , SecondActivity.class);
                 String message = edtMessage.getText().toString();
                 intent.putExtra(EXTRA_MESSAGE, message);
@@ -66,11 +103,38 @@ public class MainActivity extends AppCompatActivity {
                 mActivityLauncher.launch(intent);
             }
         });
-
-
-
     }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "on Start");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG,"on Resume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG,"on Destroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG,"on Stop");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG,"on Pause");
+
+    }
 }
--- Lap2_1_Two_Acitivities/app/src/main/java/com/example/lap2_1_two_acitivities/SecondActivity.java
+++ Lap2_1_Two_Acitivities/app/src/main/java/com/example/lap2_1_two_acitivities/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,11 +12,15 @@ import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
     public static final String EXTRA_REPLY = "hust.soict.4061.REPLY";
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
     private EditText edtReply;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "on Create");
 //  receive message from MainActivity
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
@@ -30,10 +35,44 @@ public class SecondActivity extends AppCompatActivity {
                 Intent replyInent = new Intent(SecondActivity.this, MainActivity.class);
                 replyInent.putExtra(EXTRA_REPLY,reply);
                 setResult(RESULT_OK, replyInent);
+                Log.d(LOG_TAG, "on End SecondActivity");
                 finish();
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "on Start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG,"on Resume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG,"on Destroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG,"on Stop");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG,"on Pause");
+
+    }
+
 
 
 }
